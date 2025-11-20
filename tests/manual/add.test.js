e79e9af374a878a384762ff8cda16854b41555b6
Ficelo <teiva.trillard@gmail.com>
2025-11-20 15:12:10 +0200

    Add more tests to add.test.js

--- tests/manual/add.test.js
+++ tests/manual/add.test.js
@@ -6,13 +6,29 @@ describe("add.js tests", () => {
     it("Should return 5 with 2 & 3 as input", () => {
         expect(add(2, 3)).to.equal(5);
     });
+
+    it("Should return 5 with 3 & 2 as input", () => {
+        expect(add(3, 2)).to.equal(5);
+    });
     
     it("Should return 3 with -1 & 4 as input", () => {
         expect(add(-1, 4)).to.equal(3);
     });
 
+    it("Should return 3 with 4 & -1 as input", () => {
+        expect(add(4, -1)).to.equal(3);
+    });
+
     it("Should return 0 with 0 & 0 as inputs", () => {
         expect(add(0, 0)).to.equal(0);
+    });
+
+    it("Should return 2 with 2 & 0 as inputs", () => {
+        expect(add(2, 0)).to.equal(2);
+    });
+
+    it("Should return 2 with 0 & 2 as inputs", () => {
+        expect(add(0, 2)).to.equal(2);
     });
 
     it("Should return '84' with '8' & '4' as inputs" , () => {
